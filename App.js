62bbb1d24a8979c59687d482700fef486821b2f2
Jonathan <jargumedo.aj@gmail.com>
2023-09-23 13:28:06 -0600

    Added a wrapper in the naviagator and set up the initial screen for app.

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer} from "react-navigation";
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScree";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Index: IndexScreen
+},{
+  initialRouteName: 'Index',
+  defaultNavigationOptions:{
+    title:'Blogs'
+  }
 });
+
+//App JS is expecting at least one component to be returned
+//We want to create a wrapper for the navigator by dowing the follwing
+const App =  createAppContainer(navigator);
+
+/*
+We wrap the navigator into our own component
+*/
+export default () => {
+  return <App />
+};
---
+++ src/screens/IndexScree.js
@@ -0,0 +1,14 @@
+import react from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+const IndexScreen = () => {
+    return(
+        <Text>This is the IndexScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default IndexScreen;
